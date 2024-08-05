d65cb3ea0088cd815f052f976b458e5586ef2267
Himanshu Sharma <himanshu.kumarr07@gmail.com>
2024-08-06 01:47:26 +0530

    Complete 🚩

--- index.ts
+++ index.ts
@@ -8,9 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: "http://localhost:5173",
   })
 );
 
